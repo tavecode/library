98bb07a0b6896d5a6a90ec34d9f833dcb763bfe5
Tave <131380974+tavecode@users.noreply.github.com>
2023-06-05 13:58:18 -0600

    Add basic outline

---
+++ script.js
@@ -0,0 +1,22 @@
+let library = [
+    //store books
+];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    // the constructor...
+};
+  
+  function addBookToLibrary() {
+    // do stuff here
+    
+    //Write a function that loops through the array and displays each book on the page.
+    //You can display them in some sort of table, or each on their own “card”.
+    //It might help for now to manually add a few books to your array so you can see the display.
+};
+
+
+//for in,   onscreen txtcontent, css, book variable names,
