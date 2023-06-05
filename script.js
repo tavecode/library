2ff347abcd004ace81321979136522c83ac85fa3
Tave <131380974+tavecode@users.noreply.github.com>
2023-06-05 14:42:58 -0600

    Add loop through library

--- script.js
+++ script.js
@@ -7,16 +7,32 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+    library.push(this)
     // the constructor...
 };
   
-  function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
+    new Book(title, author, pages, read);
     // do stuff here
-    
-    //Write a function that loops through the array and displays each book on the page.
-    //You can display them in some sort of table, or each on their own “card”.
-    //It might help for now to manually add a few books to your array so you can see the display.
+
 };
 
+function displayBooks() {
 
-//for in,   onscreen txtcontent, css, book variable names,
+    for (let i in library) {
+        console.log(
+            `${library[i].title}, by ${library[i].author}, ${library[i].pages} long, ${library[i].read}`
+        )
+    };
+        
+//Write a function that loops through the array and displays each book on the page.
+//You can display them in some sort of table, or each on their own “card”.
+//It might help for now to manually add a few books to your array so you can see the display.
+}
+
+
+//for in,   onscreen txtcontent, append child to bookdisplay,
+// css, book variable names,
+//  addBookToLibrary('fat', 'fatty', '23', 'no')
+
+//let i = 0; i <= library.length; i++
